0e558123e54c328dde39a239220dd3fdc0b3d9e3
jim <liu1084@163.com>
2016-11-24 00:58:49 +0800

    update

---
+++ src/components/Hello.vue
@@ -0,0 +1,17 @@
+<template>
+	<div class="hello">
+		{{msg}}
+	</div>
+</template>
+
+<script>
+	require('../assets/sass/hello.scss');
+	export default {
+		name: 'hello',
+		data: function(){
+			return {
+				msg: 'hello'
+			}
+		}
+	}
+</script>
